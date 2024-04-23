56ab967e85ea83c93196b6659fe1c2d77e765739
CNVRSN <davidaseweje@gmail.com>
2024-04-23 14:19:55 +0100

    Changed Font

--- calculator.js
+++ calculator.js
@@ -1,13 +1,33 @@
+numberBtns = document.querySelectorAll("[data-number]")
+operatorBtns = document.querySelectorAll("[data-operation]")
+equalsBtn = document.querySelector("[data-equals]")
+deleteBtn = document.querySelector("[data-delete]")
+clearBtn = document.querySelector("[data-all-clear]")
+
+
 const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
-const operate = (num1, num2, op) => op === "+" ? add((num1), (num2)) :
-                                    op === "-" ? subtract((num1), (num2)) : 
-                                    op === "*" ? multiply((num1), (num2)) :  
-                                    op === "/" ? divide((num1), (num2));
+const operate = (num1, num2, op) => op === "+" ? add(num1, num2) :
+                                    op === "-" ? subtract(num1, num2) : 
+                                    op === "*" ? multiply(num1, num2) :  
+                                    op === "/" ? divide(num1, num2);
 
-let firstNumber;
-let secondNumber;
-let operator;
+
+// operate(numberBtn1, numberBtn2, operatorBtn)
+
+// currentOperand = [] // after any operation, return to this empty value
+
+// add the input number into an array (current operand)
+numberBtns.forEach(element => {
+    button.addEventListener('click', () => {
+        appendNumber(button.innerText)
+    })
+});
+
+function appendNumber(n) {
+    if (number === '.' && currentOperand.includes('.')) return
+    currentOperand = currentOperand.toString() + n.toString()
+}e
